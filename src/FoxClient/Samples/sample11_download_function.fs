95723a40bee87a305c78c464f76fb95996c788a7
Irwin <rodriguez.irwin@gmail.com>
2021-06-30 19:34:39 +0200

    sample 11 download from internet

--- src/FoxClient/Samples/sample11_download_function.fs
+++ src/FoxClient/Samples/sample11_download_function.fs
@@ -1,31 +1,8 @@
 /*
-* FoxSharp v1.0
-* Sample 12: variadic function
-* Author: Irwin Rodriguez <rodriguez.irwin@gmail.com>
+* Download any file from internet
 */
-
-// NOTE: remember pass the last argument as variadic.
-sumAll := fn(numbers...){
-   total := 0
-   for (x in numbers){
-      total += x;
-   }
-   return total;
-};
-
-sumAll(1, 2, 3, 4, 5);
-
-// example 2
-add := fn(x, y) { return x + y; };
-sub := fn(x, y) { return x - y; };
-mul := fn(x, y) { return x * y; };
-div := fn(x, y) { return x / y; };
-
-compute := fn(x, y, functions...){
-   for (fun in functions){
-      info(fun(x, y));
-   };
-};
-
-compute(10, 2, add, sub, mul, div);
-
+if (download("https://github.com/Irwin1985/FoxSharp/edit/main/README.md", "c:\\my-folder\\README.md")){
+   info("success!");
+} else {
+   error("something went wrong!");
+}
